fef7d7ac3c3d19ece0bf700ad1379dc47a35735f
Esbjörn Blomquist <esbjorn.blomquist@cybercom.com>
2017-03-27 15:53:57 +0200

    babel etc

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # react-gauge-animated
-A physically animated Gauge as a React Component
+The [*`gauge-animated`*](https://www.npmjs.com/package/gauge-animated) as a React Component
+
+Install:
+```bash
+npm i -S react-gauge-animated
+```
+
+**[Demo of gauge-animated](http://codepen.io/whitelizard/pen/ggVoOR?editors=0010)**
+
+Example use:
+```javascript
+const options = {
+  size: 280,
+  faceText: ''
+}
+```
--- index.js
+++ index.js
@@ -1,20 +1,59 @@
-import React from 'react';
-import Gauge from 'gauge-animated';
-export { stringToElement } from 'gauge-animated';
+'use strict';
 
-export default class ReactGauge extends React.Component {
-  componentDidMount() {
-    this.gauge = new Gauge(this.div, this.props);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _gaugeAnimated = require('gauge-animated');
+
+var _gaugeAnimated2 = _interopRequireDefault(_gaugeAnimated);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var ReactGauge = function (_React$Component) {
+  _inherits(ReactGauge, _React$Component);
+
+  function ReactGauge() {
+    _classCallCheck(this, ReactGauge);
+
+    return _possibleConstructorReturn(this, (ReactGauge.__proto__ || Object.getPrototypeOf(ReactGauge)).apply(this, arguments));
   }
 
-  shouldComponentUpdate(nextProps) {
-    if (nextProps.value !== this.props.value) this.gauge.setTarget(nextProps.value);
-    return false;
-  }
+  _createClass(ReactGauge, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this.gauge = new _gaugeAnimated2.default(this.div, this.props);
+    }
+  }, {
+    key: 'shouldComponentUpdate',
+    value: function shouldComponentUpdate(nextProps) {
+      if (nextProps.value !== this.props.value) this.gauge.setTarget(nextProps.value);
+      return false;
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
 
-  render() {
-    return (
-      <div ref={c => this.div = c}></div>
-    );
-  }
-}
+      return _react2.default.createElement('div', { ref: function ref(c) {
+          return _this2.div = c;
+        } });
+    }
+  }]);
+
+  return ReactGauge;
+}(_react2.default.Component);
+
+exports.default = ReactGauge;
